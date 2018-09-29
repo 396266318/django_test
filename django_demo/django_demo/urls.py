2107d95f14f2e7b08512b17ed95771baad9931d5
玄月 <huxin39@163.com>
2018-09-29 16:55:04 +0800

    完善登录功能, 添加退出功能, project_manage 页面主功能

--- django_demo/django_demo/urls.py
+++ django_demo/django_demo/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 from user_app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('user_login/', views.login_action, name='user_login'),
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
+	path('', views.index, name='index'),
+	path('user_login/', views.login_action, name='user_login'),
+	path('accounts/login/', views.login_action, name='accounts_login'),
+	path('project_manage/', views.project_manage, name='project_manage'),
+	path('logout/', views.logout, name='logout'),
 ]
--- django_demo/user_app/views.py
+++ django_demo/user_app/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
+	"""首页"""
 	return render(request, 'index.html')
 
 
@@ -16,12 +18,25 @@ def login_action(request):
 
 		else:
 			user = auth.authenticate(username=username, password=password)
-			print(user)
-			print(type(user))
+
 			if user is not None:
-				auth.login(request, user)
-				return render(request, 'project_manage.html')
+				auth.login(request, user)  # 记录用户登录状态
+				request.session['user'] = username
+				return HttpResponseRedirect("/project_manage/")
 			else:
 				return render(request, 'index.html', {"error": "用户名或者密码错误"})
 	else:
 		return render(request, 'index.html')
+
+
+@login_required
+def project_manage(request):
+	username = request.session.get("user", '')  # 读取浏览器 session
+	return render(request, "project_manage.html", {"user": username})
+
+
+def logout(request):
+	auth.logout(request)  # 清除用户登录状态
+	response = HttpResponseRedirect('/')
+	# response = redirect(reversed('index.html'))
+	return response
