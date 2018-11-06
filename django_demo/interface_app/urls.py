20f72cc5630b73fc8e6a61a7f71b4071096bae7a
玄月 <huxin39@163.com>
2018-11-06 18:47:22 +0800

    interface_app 调试提交

--- django_demo/interface_app/urls.py
+++ django_demo/interface_app/urls.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 author: Xuan
-time: 2018/11/4 10:02 PM
+time: 2018/11/6 3:45 PM
 """
 from django.urls import path
 from interface_app import views
 
-urlpatterns = [
-	# 项目管理
-	path('case_manage/', views.case_manage),
 
+urlpatterns = [
+	# 用例管理
+	path("case_manage/", views.case_manage),
+	path("debug/", views.debug),
+	path("api_debug/", views.api_debug),
 ]
